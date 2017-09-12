2ed49eb596dcbdccb0284112a3c4d77adec4f34b
joaolandino <joaopaulohildebrand@gmail.com>
2017-09-12 12:12:06 -0300

    Começando a adicionar rotas no projeto

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
 import Grid from 'material-ui/Grid';
+import List, { ListItem, ListItemText } from 'material-ui/List';
 import Bitcoin from '../Bitcoin/Bitcoin';
+import Paper from 'material-ui/Paper';
+
 
 class App extends Component {
     render() {
         return (
-            <div id="app">
-                <Grid container>
+            <Router>
+                <div id="app">
+                    <Grid container>
 
-                    <Grid item xs={12}>
-                        <Bitcoin></Bitcoin>
+                        <Grid item xs={12}>
+                            <Bitcoin></Bitcoin>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Paper>
+                                <List>
+                                    <NavLink to="/lista">
+                                        <ListItem button>
+                                            <ListItemText primary="Lista de Compras"></ListItemText>
+                                        </ListItem>
+                                    </NavLink >
+                                </List>
+                            </Paper>
+                        </Grid>
+
+
+                        <Grid item xs={12}>
+                            <Route path="/lista" render={() => <div>Lista</div>} />
+                        </Grid>
+
                     </Grid>
-
-                </Grid>
-            </div>
+                </div>
+            </Router>
         );
     }
 }
