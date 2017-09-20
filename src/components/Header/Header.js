3258fde950baa4b94114a70cb77fb791934e760d
João Paulo Hildebrand Landino <joao.landino@DMCONTROLE>
2017-09-20 13:54:44 -0300

    Drawer adicionado ao componente Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
+import Drawer from 'material-ui/Drawer';
 import MenuIcon  from 'material-ui-icons/Menu';
+import PersonPin  from 'material-ui-icons/PersonPin';
 import { withStyles } from 'material-ui/styles';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
 
 
 const styles = theme => ({
@@ -16,16 +20,60 @@ const styles = theme => ({
     },
 });
 
-function Header(props){
-    const classes = props.classes;
 
-    return(
-        <AppBar position='static' elevation={0} className={classes.AppBar}>
-            <Toolbar className={classes.Toolbar}>
-                <MenuIcon />
-            </Toolbar>
-        </AppBar>
-    );
+class Header extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.classes = props.classes;
+
+        this.state = {
+            drawerState: false
+        };
+    }
+
+    handleDrawer(){
+        this.setState({
+            drawerState: !this.state.drawerState
+        });
+    }
+
+    render(){
+        return(
+            <div>
+
+                <AppBar
+                    position='static'
+                    elevation={0}
+                    className={this.classes.AppBar}
+                    >
+
+                    <Toolbar className={this.classes.Toolbar}>
+                        <MenuIcon onClick={this.handleDrawer.bind(this)} />
+                    </Toolbar>
+                </AppBar>
+
+                <Drawer
+                    type="temporary"
+                    open={this.state.drawerState}
+                    onRequestClose={this.handleDrawer.bind(this)}
+                    >
+
+                    <List>
+                        <ListItem button component="a" href="http://github.com/joaolandino" target="_blank">
+                            <ListItemIcon>
+                                <PersonPin />
+                            </ListItemIcon>
+                            <ListItemText primary="Sobre o Desenvolvedor" secondary="github.com/joaolandino" />
+                        </ListItem>
+                    </List>
+                </Drawer>
+
+            </div>
+        );
+    }
+
 }
 
 export default withStyles(styles)(Header);
