014242214fb47f2f94416c822c9c2a0b10b6788a
João Paulo Hildebrand Landino <joao.landino@DMCONTROLE>
2017-10-17 17:52:20 -0200

    Avanços no clique do item do menu de seleção de preços

--- src/components/Lista/Lista.js
+++ src/components/Lista/Lista.js
@@ -4,107 +4,122 @@ import Grid from 'material-ui/Grid';
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import Menu, { MenuItem } from 'material-ui/Menu';
 
+
+const precos = [
+  0.50,
+  1.00,
+  1.50,
+  2.00,
+  2.50,
+  3.00,
+  3.50,
+  4.00,
+  4.50,
+  5.00,
+  5.50,
+  6.00,
+  6.50,
+  7.00,
+  7.50,
+  8.00,
+  8.50,
+  9.00,
+  9.50,
+  10.00
+];
+
+
 class Lista extends Component {
 
-    constructor(props){
-        super(props);
+  constructor(props){
+      super(props);
 
-        this.listaCompra = [
-            {
-              'codigo': '1',
-              'produto': 'Coca-Cola',
-              'quantidade': 1
-            },
-            {
-              'codigo': '2',
-              'produto': 'Arroz',
-              'quantidade': 1
-            }
-        ];
+      this.listaCompra = [
+          {
+            'produto': 'Coca-Cola',
+            'quantidade': 1
+          },
+          {
+            'produto': 'Arroz',
+            'quantidade': 1
+          }
+      ];
 
-        this.state = {
-          menuOpen: false,
-          menuAnchor: null
-        }
-    }
+      this.state = {
+        menuOpen: false,
+        menuAnchor: null
+      }
+  }
 
 
-    componentWillUnmount(){
-        this.listaCompra = [];
-    }
+  componentWillUnmount(){
+      this.listaCompra = [];
+  }
 
 
-    handleClickListItem(event){
-      this.setState({
-        menuOpen: true,
-        menuAnchor: event.currentTarget
-      });
-    }
+  handleClickListItem(event){
+    this.setState({
+      menuOpen: true,
+      menuAnchor: event.currentTarget
+    });
+  }
 
 
-    handleRequestClose(){
-      this.setState({
-        menuOpen: false
-      });
-    }
+  handleRequestClose(){
+    this.setState({
+      menuOpen: false
+    });
+  }
 
 
-    render(){
-        return (
-            <List>
-                <form action="#" method="post">
+  selectItem(_keyPreco, _keyItem){
+    console.log(precos[_keyPreco]);
+    console.log(_keyItem);
+  }
 
-                    {
-                        this.listaCompra.map(item => {
-                            return (
-                                <ListItem disableGutters divider={true} key={item.codigo}>
-                                    <Grid container spacing={0} alignItems="center">
 
-                                        <Grid item xs={8} md={10}>
-                                            <ListItemText primary={item.produto} secondary={"Quantidade: " + item.quantidade} />
-                                        </Grid>
+  render(){
+      return (
+          <List>
+              <form action="#" method="post">
 
-                                        <Grid item xs={4} md={2} className="centered">
-                                          <List dense={true}>
-                                            <ListItem disableGutters button onClick={this.handleClickListItem.bind(this)}>
-                                              <ListItemText primary="Preço" secondary="R$00,00" className="ListItemText" />
-                                            </ListItem>
-                                          </List>
-                                          <Menu anchorEl={this.state.menuAnchor} open={this.state.menuOpen} onRequestClose={this.handleRequestClose.bind(this)} transitionDuration={0}>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$0,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$1,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$1,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$2,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$2,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$3,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$3,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$4,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$4,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$5,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$5,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$6,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$6,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$7,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$7,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$8,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$8,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$9,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$9,50</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$10,00</MenuItem>
-                                              <MenuItem onClick={this.handleRequestClose.bind(this)}>R$10,50</MenuItem>
-                                          </Menu>
-                                        </Grid>
+                  {
+                      this.listaCompra.map((_item, _keyItem) => {
+                          return (
+                              <ListItem disableGutters divider={true} key={_keyItem}>
+                                  <Grid container spacing={0} alignItems="center">
 
-                                    </Grid>
-                                </ListItem>
-                            )
-                        })
-                    }
+                                      <Grid item xs={8} md={10}>
+                                          <ListItemText primary={_item.produto} secondary={"Quantidade: " + _item.quantidade} />
+                                      </Grid>
 
-                </form>
-            </List>
-        );
-    }
+                                      <Grid item xs={4} md={2} className="centered">
+                                        <List dense={true}>
+                                          <ListItem disableGutters button onClick={this.handleClickListItem.bind(this)}>
+                                            <ListItemText primary="Preço" secondary="R$00,00" className="ListItemText" />
+                                          </ListItem>
+                                        </List>
+                                        <Menu anchorEl={this.state.menuAnchor} open={this.state.menuOpen} onRequestClose={this.handleRequestClose.bind(this)} transitionDuration={0}>
+                                          {
+                                            precos.map((_preco, _keyPreco) => {
+                                              return (
+                                                <MenuItem key={_keyPreco} onClick={() => { this.handleRequestClose(); this.selectItem(_keyPreco, _item); }}>R${_preco}</MenuItem>
+                                              )
+                                            })
+                                          }
+                                        </Menu>
+                                      </Grid>
+
+                                  </Grid>
+                              </ListItem>
+                          )
+                      })
+                  }
+
+              </form>
+          </List>
+      );
+  }
 }
 
 export default Lista;
